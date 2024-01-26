12c94533c757b951ba31f85c2b7e5be476007e55
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-01-26 14:19:05 -0600

    dll
    dll

---
+++ doubly_linked_list.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const dll = new DoublyLinkedList();
+dll.push("25");
+dll.push("50");
+dll.push("75");
+dll.push("100");
+dll.push("125");
+dll.push("150");
+dll.push("175");
+dll.push("200");
