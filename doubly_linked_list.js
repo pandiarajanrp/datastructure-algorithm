732c1b4326f3c273c6da04115f45ac9ed783cc9b
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-01-29 17:21:06 -0600

    dll pop and shift
    dll pop and shift

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -25,6 +25,36 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if(!this.head) return null;
+    const removedItem = this.tail;
+    
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedItem.prev;
+      this.tail.next = null;
+      removedItem.prev = null;
+    }
+   
+    this.length--;
+    return removedItem;
+  }
+  shift() {
+    if (!this.head) return null;
+    const removedItem = this.head; 
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removedItem.next;
+      this.head.prev = null;
+      removedItem.next = null;
+    }
+    this.length--;
+    return removedItem
+  }
 }
 
 const dll = new DoublyLinkedList();
@@ -32,7 +62,13 @@ dll.push("25");
 dll.push("50");
 dll.push("75");
 dll.push("100");
-dll.push("125");
-dll.push("150");
-dll.push("175");
-dll.push("200");
+// dll.push("125");
+// dll.push("150");
+// dll.push("175");
+// dll.push("200");
+
+//pop
+//dll.pop()
+
+//shift
+dll.shift()
