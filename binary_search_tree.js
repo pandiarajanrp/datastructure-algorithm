325b5edee1a00f4c48031168dc564bb34ef084d5
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-13 13:53:03 -0600

    bst find
    bst find

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -1,50 +1,67 @@
 class Node {
   constructor(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+      this.value = val;
+      this.left = null;
+      this.right = null;
   }
 }
 
 class BST {
   constructor() {
-    this.root = null;
+      this.root = null;
   }
-  insert(value) {
-    const node = new Node(value);
-    if (!this.root) {
-      this.root = node;
-      return node;
-    }
 
-    //check left node
-    let current = this.root;
-    while (true) {
-      //check in left
-      if (value < current.value) {
-        if (!current.left) {
-          current.left = node;
-          return this;
-        }
-        current = current.left;
-        // check in the right
-      } else if (value > current.value) {
-        if (!current.right) {
-          current.right = node;
-          return this;
-        }
-        current = current.right;
-        // check the value already exist
-      } else {
-        return null;
+  insert(value) {
+      const node = new Node(value)
+      if (!this.root) {
+          this.root = node;
+          return node;
       }
-    }
+
+      //check left node
+      let current = this.root;
+      while(true) {
+          //check in left
+         if (value < current.value) {
+             if (!current.left) {
+                 current.left = node;
+                 return this;
+             }
+             current = current.left;
+         // check in the right
+         } else if(value > current.value) {
+             if (!current.right) {
+                current.right = node;
+                return this;
+             }
+             current = current.right;
+         // check the value already exist
+         } else {
+             return null;
+         }
+      }
+  }
+  find(value) {
+      if (!this.root) {
+          return null;
+      }
+      let current = this.root;
+      while(true) {
+          if (current.value === value) return value;
+          if (value < current.value) {
+              if (!current.left) return null;
+              current = current.left;
+          } else if(value > current.value) {
+              if (!current.right) return null;
+              current = current.right;
+          }
+      }
   }
 }
 
 const bst = new BST();
 
-bst.insert(10);
+bst.insert(10)
 bst.insert(5);
 bst.insert(15);
 bst.insert(4);
